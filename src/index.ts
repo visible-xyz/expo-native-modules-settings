0ee20190dd225fc5a4a7c3e13a20674588f904cd
Denys Maksymenko <denys@visible.xyz>
2024-11-19 14:35:53 +0000

    app delegate fix

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,9 @@ import NativeModulesSettings from "./NativeModulesSettings";
 
 // Get the native constant value.
 export const PI = NativeModulesSettings.PI;
+
+export function hello(): string {
+  return NativeModulesSettings.hello();
+}
+
+export * from "react-native-appsflyer";
