58af387eb9d88e8269795e67f9420211fd1971cf
Denys Maksymenko <denys@visible.xyz>
2024-11-20 15:08:39 +0000

    fix

--- android/src/main/java/xyz/visible/visiblerentapp/NativeModulesSettings.kt
+++ android/src/main/java/xyz/visible/visiblerentapp/NativeModulesSettings.kt
@@ -3,7 +3,7 @@ package xyz.visible.visiblerentapp
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
-class NativeModulesSettingsModule : Module() {
+class NativeModulesSettings : Module() {
   // Each module class must implement the definition function. The definition consists of components
   // that describes the module's functionality and behavior.
   // See https://docs.expo.dev/modules/module-api for more details about available components.
